df74fe0e9c59e1df7113113bdfd5c13d02ec58d5
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-03-02 19:18:29 -0300

    feat: rotas de login e autenticação

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('authenticate', [LoginController::class, 'authenticate'])->name('authenticate');
