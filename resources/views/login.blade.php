d2cfd461a6a6e032ce6f305fcc6c6378a36480ca
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-03-02 19:18:05 -0300

    feat: tela de login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,15 @@
+<form action="/authenticate" method="post">
+    @csrf
+
+    <label for="email">Email</label>
+    <input type="text" id="email" name="email">
+    <br>
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password">
+    <br>
+    <input type="submit" value="Login">
+</form>
+
+@if (session('message'))
+    <div>{{ session('message') }}</div>
+@endif
