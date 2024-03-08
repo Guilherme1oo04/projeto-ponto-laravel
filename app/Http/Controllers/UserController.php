821bc05bfb8a01c500c64d761c64514d2b7b4c81
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-03-08 17:36:17 -0300

    feat: user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class PontoController extends Controller
+class UserController extends Controller
 {
-    public function index() {
-        return "Deu certo kkkkkkkk!";
+    public function home() {
+        return view('user.home');
     }
 }
