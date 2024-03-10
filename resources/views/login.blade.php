6d44a72ffbdc371b865ce7bc221867b6fb47fecd
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-03-10 19:18:47 -0300

    feat: customizing login page css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,15 +1,44 @@
-<form action="{{route('auth.authenticate')}}" method="post">
-    @csrf
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    @vite('resources/css/app.css')
+    @vite('resources/css/login.css')
 
-    <label for="email">Email</label>
-    <input type="text" id="email" name="email">
-    <br>
-    <label for="password">Password</label>
-    <input type="password" id="password" name="password">
-    <br>
-    <input type="submit" value="Login">
-</form>
+</head>
+<body>
+    
+    <div class="screen">
 
-@if (session('message'))
-    <div>{{ session('message') }}</div>
-@endif
+        <h1 class="title">Clock In Help</h1>
+
+        <form action="{{route('auth.authenticate')}}" method="post" class="">
+            @csrf
+        
+            <div class="inputs">
+                <input type="text" id="email" name="email" class="input" placeholder=" ">
+                <label for="email" class="input-label">
+                    Email
+                </label>
+            </div>
+            <div class="inputs">
+                <input type="password" id="password" name="password" class="input" placeholder=" ">
+                <label for="password" class="input-label">
+                    Password
+                </label>
+            </div>
+        
+            <input type="submit" value="Login">
+        </form>
+        
+        @if (session('message'))
+            <div>{{ session('message') }}</div>
+        @endif
+    </div>
+
+
+</body>
+</html>
