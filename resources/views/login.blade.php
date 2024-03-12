242caf97eb46623b942c78044ffd35e31646896e
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-03-12 18:59:14 -0300

    feat: add style to login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,42 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
     @vite('resources/css/app.css')
-    @vite('resources/css/login.css')
-
 </head>
-<body>
-    
-    <div class="screen">
+<body class="tw-bg-white">
 
-        <h1 class="title">Clock In Help</h1>
+    <div class="tw-flex tw-min-h-[100vh] tw-justify-around tw-flex-col tw-items-center tw-mx-10">
 
-        <form action="{{route('auth.authenticate')}}" method="post" class="">
-            @csrf
-        
-            <div class="inputs">
-                <input type="text" id="email" name="email" class="input" placeholder=" ">
-                <label for="email" class="input-label">
-                    Email
-                </label>
-            </div>
-            <div class="inputs">
-                <input type="password" id="password" name="password" class="input" placeholder=" ">
-                <label for="password" class="input-label">
-                    Password
-                </label>
-            </div>
-        
-            <input type="submit" value="Login">
-        </form>
-        
-        @if (session('message'))
-            <div>{{ session('message') }}</div>
-        @endif
+        <h1 class="tw-text-3xl tw-font-bold tw-text-center tw-text-gray-900">Sign in to your acount</h1>
+
+        <div class="tw-w-full tw-max-w-sm tw-h-96">
+            <form action="{{route('auth.authenticate')}}" method="post" class="tw-flex tw-flex-col">
+                @csrf
+
+                <div class="tw-mb-6">
+                    <label for="email" class="tw-text-md tw-font-medium tw-text-gray-900 tw-block tw-mb-2">Email address</label>
+                    <input type="email" required name="email" id="email" class="tw-w-full tw-bg-transparent tw-border tw-border-solid tw-border-gray-300 tw-rounded-md tw-h-9 tw-p-3 tw-text-lg tw-font-normal tw-cursor-text tw-outline-none focus:tw-border-blue-600 focus:tw-border-2">
+                </div>
+
+                <div>
+                    <div class="tw-flex tw-justify-between tw-mb-2">
+                        <label for="password" class="tw-text-md tw-font-medium tw-text-gray-900">Password</label>
+                        <a href="#" class="tw-text-md tw-font-medium tw-text-blue-600 hover:tw-text-blue-700">Forgot password?</a>
+                    </div>
+                    <input type="password" required name="password" id="password" class="tw-w-full tw-bg-transparent tw-border tw-border-solid tw-border-gray-300 tw-rounded-md tw-h-9 tw-p-3 tw-text-lg tw-font-normal tw-cursor-text tw-outline-none focus:tw-border-blue-600 focus:tw-border-2">
+                </div>
+
+                <input type="submit" value="Sign in" class="tw-w-28 tw-mx-auto tw-mt-5 tw-bg-blue-600 tw-text-xl tw-py-1 tw-cursor-pointer tw-font-semibold tw-rounded-3xl tw-text-white hover:tw-bg-blue-700">
+            </form>
+
+
+            @if (session('message'))
+                <p class="tw-text-center tw-mt-5 tw-text-xl tw-text-red-500 tw-font-semibold">{{ session('message') }}</p>
+            @endif
+            
+        </div>
+
     </div>
-
-
+    
 </body>
 </html>
