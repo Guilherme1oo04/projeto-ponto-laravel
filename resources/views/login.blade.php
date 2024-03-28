ae371dcff761d27f4c115680f8d3b02e8fe91799
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-03-28 19:27:30 -0300

    feat: login page refatoring css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,35 +6,56 @@
     <title>Login</title>
     @vite('resources/css/app.css')
 </head>
-<body class="tw-bg-white">
+<body class="tw-bg-gray-100">
+    <h1 class="tw-text-center tw-py-2 tw-text-lg tw-flex tw-items-center tw-mx-auto tw-justify-center tw-font-semibold tw-text-gray-800 tw-cursor-default">
+        <img src="../assets/icon-clock.svg" class="tw-w-10 tw-mr-2">
+        Clock In Help
+    </h1>
 
-    <div class="tw-flex tw-min-h-[100vh] tw-justify-around tw-flex-col tw-items-center tw-mx-10">
+    <div class="tw-flex tw-min-h-[85vh] tw-justify-center tw-items-center tw-mx-10">
 
-        <h1 class="tw-text-3xl tw-font-bold tw-text-center tw-text-slate-900">Sign in to your acount</h1>
+        <div class="tw-w-full tw-max-w-md tw-h-[500px] tw-bg-white tw-border tw-border-gray-300 tw-shadow-md tw-rounded-lg tw-px-3 tw-py-4">
 
-        <div class="tw-w-full tw-max-w-sm tw-h-96">
+            <div class="tw-py-5">
+                <h1 class="tw-text-2xl tw-font-bold tw-text-center tw-text-gray-800 tw-block">Sign in to your acount</h1>
+            </div>
+
+            <hr class="tw-mb-5">
+
             <form action="{{route('auth.authenticate')}}" method="post" class="tw-flex tw-flex-col">
                 @csrf
 
-                <div class="tw-mb-6">
-                    <label for="email" class="tw-text-md tw-font-medium tw-text-slate-900 tw-block tw-mb-2">Email address</label>
-                    <input type="email" required name="email" id="email" class="tw-w-full tw-bg-transparent tw-border tw-border-solid tw-border-slate-300 tw-rounded-md tw-h-9 tw-p-3 tw-text-lg tw-font-normal tw-cursor-text tw-outline-none focus:tw-border-slate-700 focus:tw-border-2">
+                <div class="tw-flex tw-flex-col tw-px-7 tw-mb-4">
+                    <label for="email" class="tw-text-gray-900 tw-mb-1">Email</label>
+                    <input type="email" required name="email" id="email" class="tw-border tw-rounded-md tw-px-3 tw-py-2 tw-text-gray-900 tw-outline-none focus:tw-border-2 focus:tw-border-blue-700">
                 </div>
 
-                <div>
-                    <div class="tw-flex tw-justify-between tw-mb-2">
-                        <label for="password" class="tw-text-md tw-font-medium tw-text-slate-900">Password</label>
-                        <a href="#" class="tw-text-md tw-font-medium tw-text-slate-500 hover:tw-text-slate-700 hover:tw-underline">Forgot password?</a>
-                    </div>
-                    <input type="password" required name="password" id="password" class="tw-w-full tw-bg-transparent tw-border tw-border-solid tw-border-slate-300 tw-rounded-md tw-h-9 tw-p-3 tw-text-lg tw-font-normal tw-cursor-text tw-outline-none focus:tw-border-slate-700 focus:tw-border-2">
+                <div class="tw-flex tw-flex-col tw-px-7 tw-mb-4">
+                    <label for="password" class="tw-text-gray-900 tw-mb-1">Password</label>
+                    <input type="password" name="password" id="password" class="tw-border tw-rounded-md tw-px-3 tw-py-2 tw-text-gray-900 tw-outline-none focus:tw-border-2 focus:tw-border-blue-700">
                 </div>
 
-                <input type="submit" value="Sign in" class="tw-w-28 tw-mx-auto tw-mt-5 tw-bg-slate-700 tw-text-xl tw-py-1 tw-cursor-pointer tw-font-semibold tw-rounded-3xl tw-text-white hover:tw-bg-slate-800">
+                <input type="submit" value="Sign in" class="tw-w-28 tw-mx-auto tw-mt-4 tw-bg-blue-700 tw-text-xl tw-py-1 tw-cursor-pointer tw-font-semibold tw-rounded-3xl tw-text-white hover:tw-bg-blue-900">
             </form>
+
+            <p class="tw-px-7 tw-text-center tw-mt-5 tw-text-md">
+                Forgot password?
+                <a href="#" class="tw-text-blue-700 tw-font-semibold tw-outline-none hover:tw-underline">
+                    Click here
+                </a>
+            </p>
 
 
             @if (session('message'))
-                <p class="tw-text-center tw-mt-5 tw-text-xl tw-text-red-500 tw-font-semibold">{{ session('message') }}</p>
+                <p id="session-message" class="tw-text-center tw-mt-7 tw-mx-7 tw-py-3 tw-rounded-md tw-text-xl tw-bg-red-500 tw-text-white tw-font-semibold">{{ session('message') }}</p>
+
+                <script>
+                    const messageParagraph = document.getElementById('session-message');
+
+                    setTimeout(() => {
+                        messageParagraph.style.display = 'none';
+                    }, 4000);
+                </script>
             @endif
             
         </div>
