ad092864f7085dd4d1974fc00a8802619e3845aa
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-04-03 19:24:54 -0300

    feat: add route in the form

--- resources/views/superAdmin/timelines.blade.php
+++ resources/views/superAdmin/timelines.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 
     <div class="tw-w-screen tw-flex tw-justify-center tw-items-center tw-py-5">
-        <a href="#" class="tw-font-semibold tw-text-lg tw-text-white tw-bg-blue-700 tw-py-2 tw-px-3 tw-rounded-3xl tw-flex tw-justify-center tw-items-center tw-shadow-md tw-duration-200 tw-ease-linear hover:tw-bg-blue-800 hover:tw-shadow-lg">
+        <a href="{{route('admin.timelines.add')}}" class="tw-font-semibold tw-text-lg tw-text-white tw-bg-blue-700 tw-py-2 tw-px-3 tw-rounded-3xl tw-flex tw-justify-center tw-items-center tw-shadow-md tw-duration-200 tw-ease-linear hover:tw-bg-blue-800 hover:tw-shadow-lg">
             <span class="material-symbols-outlined tw-mr-1">
                 add
             </span>
-            Add timeline
+            Nova timeline
         </a>
     </div>
     
