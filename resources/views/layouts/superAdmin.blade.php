b897ef6615bc268361cf84a7015cec976acd2225
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-04-20 18:52:56 -0300

    feat: add transition to mobile menu

--- resources/views/layouts/superAdmin.blade.php
+++ resources/views/layouts/superAdmin.blade.php
@@ -70,13 +70,13 @@
         <nav class="md:tw-hidden" x-data="{open: false}" @click.away="open=false">
 
             <button @click="open = !open">
-                <svg x-show="!open" class="tw-w-9 tw-h-9 tw-mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" viewBox="0 0 24 24" stroke="currentColor">
+                <svg x-show="!open" class="tw-w-9 tw-h-9" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" viewBox="0 0 24 24" stroke="currentColor">
                     <path d="M4 6h16M4 12h16M4 18h16"></path>
                 </svg>
-                <svg x-show="open" class="tw-w-9 tw-h-9 tw-mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" viewBox="0 0 24 24" stroke="currentColor"><path d="M6 18L18 6M6 6l12 12"></path></svg>
+                <svg x-show="open" class="tw-w-9 tw-h-9" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" viewBox="0 0 24 24" stroke="currentColor"><path d="M6 18L18 6M6 6l12 12"></path></svg>
             </button>
 
-            <div x-show="open" id="menu-mobile" style="display: none" class="tw-flex tw-flex-col tw-absolute tw-bg-white tw-rounded-md tw-right-4 tw-translate-y-12 tw-items-end tw-shadow-md tw-justify-around tw-w-36">
+            <div x-show="open" x-transition:enter="tw-ease-out tw-duration-200" x-transition:enter-start="tw--translate-y-24" x-transition:enter-end="tw-translate-y-12" x-transition:leave="tw-ease-in tw-duration-200" x-transition:leave-start="tw-translate-y-12" x-transition:leave-end="tw--translate-y-24" id="menu-mobile" class="tw-flex tw-flex-col tw-absolute tw-bg-white tw-rounded-md tw-right-4 tw-translate-y-12 tw-items-end tw-shadow-md tw-justify-around tw-w-36">
                 <a href="{{route('admin.home')}}" class="tw-duration-200 tw-ease-in tw-text-md tw-text-gray-800 tw-font-semibold tw-rounded-t-md tw-px-2 tw-py-4 tw-w-full hover:tw-bg-blue-700 hover:tw-text-white tw-flex tw-items-center">
                     <span class="material-symbols-outlined tw-mr-2">
                         home
