b3294d1de5567397b74b86f60fcc5fd38276ddd4
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-05-12 18:15:04 -0300

    chore: adjusting menu animation

--- resources/views/layouts/superAdmin.blade.php
+++ resources/views/layouts/superAdmin.blade.php
@@ -76,7 +76,7 @@
                 <svg x-show="open" class="tw-w-9 tw-h-9" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" viewBox="0 0 24 24" stroke="currentColor"><path d="M6 18L18 6M6 6l12 12"></path></svg>
             </button>
 
-            <div x-show="open" x-transition:enter="tw-ease-out tw-duration-200" x-transition:enter-start="tw--right-24" x-transition:enter-end="tw-right-4" x-transition:leave="tw-ease-in tw-duration-200" x-transition:leave-start="tw-right-4" x-transition:leave-end="tw--right-24" id="menu-mobile" class="tw-flex tw-flex-col tw-absolute tw-bg-white tw-rounded-md tw-right-4 tw-translate-y-12 tw-items-end tw-shadow-md tw-justify-around tw-w-36">
+            <div x-show="open" x-transition:enter="tw-ease-out tw-duration-200" x-transition:enter-start="tw--right-20" x-transition:enter-end="tw-right-4" x-transition:leave="tw-ease-in tw-duration-200" x-transition:leave-start="tw-right-4" x-transition:leave-end="tw--right-20" id="menu-mobile" class="tw-flex tw-flex-col tw-absolute tw-bg-white tw-rounded-md tw-right-4 tw-translate-y-12 tw-items-end tw-shadow-md tw-justify-around tw-w-36">
                 <a href="{{route('admin.home')}}" class="tw-duration-200 tw-ease-in tw-text-md tw-text-gray-800 tw-font-semibold tw-rounded-t-md tw-px-2 tw-py-4 tw-w-full hover:tw-bg-blue-700 hover:tw-text-white tw-flex tw-items-center">
                     <span class="material-symbols-outlined tw-mr-2">
                         home
